d1e7bdff2b2aa5881030124bc80566068b40e565
Maiyer Thao <MJAT@Maiyers-iMac.local>
2018-01-18 16:03:58 -0600

    finished w/ styling, adding logic

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCxkO8Bafmy7YQZNGBNYLfg73qxFDeYd1U",
+    authDomain: "training-day-13ca7.firebaseapp.com",
+    databaseURL: "https://training-day-13ca7.firebaseio.com",
+    projectId: "training-day-13ca7",
+    storageBucket: "training-day-13ca7.appspot.com",
+    messagingSenderId: "197932357333"
+  };
+  firebase.initializeApp(config);
